7ddb3d2c9a2f0588588be1c6aed829b110f99203
juarezmeneses <juarezlmfilho@gmail.com>
2022-09-16 15:56:13 -0300

    StatelessWidget

--- app_quiz_rick_morty/lib/main.dart
+++ app_quiz_rick_morty/lib/main.dart
@@ -1,13 +1,28 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Quiz Rick and Morty',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 40.0),
-      ),
-    ),
+  runApp(AppWidget(
+    title: 'Quiz Rick and Morty',
   ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 40.0),
+        ),
+      ),
+    );
+  }
 }
