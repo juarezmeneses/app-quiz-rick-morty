44e7c5625940b46f3b9037b768e40ffe9645637c
juarezmeneses <juarezlmfilho@gmail.com>
2022-09-16 16:51:34 -0300

    StatefulWidget

--- app_quiz_rick_morty/lib/main.dart
+++ app_quiz_rick_morty/lib/main.dart
@@ -17,13 +17,36 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.red),
-      home: Container(
-        child: Center(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: GestureDetector(
           child: Text(
-            title,
+            'Quiz Rick and Morty: $counter',
             textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 30.0),
+            style: TextStyle(fontSize: 25.0),
           ),
+          onTap: () {
+            setState(() {
+              counter++;
+            });
+          },
         ),
       ),
     );
